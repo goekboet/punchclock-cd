e997d39073071747ac3b55a316e4fa917854852c
Erik Göök <erikgook@gmail.com>
2019-02-16 21:23:01 +0100

    triggers build-pipeline

--- printMySp/script.sh
+++ printMySp/script.sh
@@ -1,3 +1,3 @@
-#! /bin/sh
-
-az account show
+az group create --location westeurope --name punchclock 
+az appservice plan create --name punchclock --resource-group punchclock --sku FREE 
+az webapp create --name punchclock-api --resource-group punchclock --plan punchclock
